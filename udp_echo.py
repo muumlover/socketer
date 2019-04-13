6ff732fd60313a9db5133f3c7f6c1906b76ce9d6
muumlover <muumlover@live.com>
2019-04-13 17:01:16 +0800

    Update udp_echo.py

--- udp_echo.py
+++ udp_echo.py
@@ -3,7 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 s.bind(('0.0.0.0', 12345))
-print 'Bind UDP on 12345...'
+print('Bind UDP on 12345...')
 while True:
     data, addr = s.recvfrom(1024)
     s.sendto(data, addr)
